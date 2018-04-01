0829f97f59c5c5244c01cd60105327ea459ff1e5
Victor Makoed <vmakoed@gmail.com>
2018-04-02 01:03:19 +0300

    Project structure, eslint, estylesheet

---
+++ app/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from '../components/Container';
+
+const Home = () => (
+  <Container>
+    <Text>Hello world</Text>
+  </Container>
+);
+
+export default Home;
